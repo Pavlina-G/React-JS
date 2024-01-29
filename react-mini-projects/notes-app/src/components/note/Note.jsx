4c2fb94d10ad5f269c59e28121bf07d83a2941a3
pavlina <102150683+Pavlina-G@users.noreply.github.com>
2024-01-29 16:24:40 +0200

    Update NoteComponent - adding Edit and Delete buttons

--- react-mini-projects/notes-app/src/components/note/Note.jsx
+++ react-mini-projects/notes-app/src/components/note/Note.jsx
@@ -1,13 +1,22 @@
+import Button from '../button/Button';
 import './Note.css'
 
 import React from 'react'
 
 function Note(props) {
-  const { id, name} = props;
+  const { id, name, editNote, deleteNote } = props;
+
   return (
-    <li key={id} className='container'>
-      <p>{name}</p>
-    </li>
+    <div className='note-container'>
+      <p key={id} className=''>{name}</p>
+      <div className="buttons-wrapper">
+        <Button text={'Edit'} handleClick={editNote} />
+        <Button text={'Delete'} handleClick={deleteNote} />
+        </div>
+
+    </div>
+
+
   )
 }
 
