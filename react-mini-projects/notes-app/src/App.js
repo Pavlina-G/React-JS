50bb5ad521e5322e263cf7866c8dda870d0f6569
pavlina <102150683+Pavlina-G@users.noreply.github.com>
2024-01-29 16:23:42 +0200

    Adding AddNote component

--- react-mini-projects/notes-app/src/App.js
+++ react-mini-projects/notes-app/src/App.js
@@ -2,20 +2,23 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header';
-import NotesApp from './pages/notes-app/NotesApp';
 import AddNote from './pages/add-notes/AddNote';
+import NotesList from './components/notes-list/NotesList';
+import { useState } from 'react';
+
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
   return (
     <div className="App">
       <Header />
 
-
       {/* Page urls */}
 
       <Routes>
-        <Route path='/' element={<NotesApp />} />
-        <Route path='/add-note' element={<AddNote />} />
+        <Route path='/' element={<NotesList notes={notes} setNotes={setNotes}/>} />
+        <Route path='/add-note' element={<AddNote setNotes={setNotes}/>} />
       </Routes>
 
       {/* Page urls */}
---
+++ react-mini-projects/notes-app/src/pages/add-notes/AddNote.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './AddNote.css'
+import Button from '../../components/button/Button';
+import { useNavigate } from 'react-router-dom';
+
+
+let num = 0;
+const AddNote = ({ setNotes }) => {
+
+    const [name, setName] = useState('');
+    const navigate = useNavigate();
+
+    function getKeyId() {
+        let id = num + 1;
+        num += 1;
+        return id
+    }
+
+    const handleBtnClick = (e) => {
+        e.preventDefault()
+        if (name) {
+            const note = {
+                id: getKeyId(),
+                name
+            }
+            console.log(setNotes);
+            setNotes(prevNotes => [note, ...prevNotes])
+            setName('');
+            navigate('/')
+        }
+    }
+
+    return (
+        <div className="add-note">
+            <h1>Add New Note</h1>
+            <div className='input-form-wrapper'>
+                <textarea rows="4" value={name} placeholder='Add Note' onChange={(e) => setName(e.target.value)}></textarea>
+                <Button text={'Add New Note'} handleClick={handleBtnClick} />
+            </div>
+            <div className='notes-img'></div>
+
+        </div>
+    )
+}
+export default AddNote;
